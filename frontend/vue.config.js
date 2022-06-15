faae7a6718eb1f957b198644d48be2b66fb92dbd
a.vajsert <a.vajsert18@yahoo.de>
2022-06-15 20:08:02 +0200

    frontend target url updated

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:8081',
+        target: 'http://server.it-humke.de:9002',
         ws: true,
         changeOrigin: true,
         pathRewrite: {'^/api':'/api'},
